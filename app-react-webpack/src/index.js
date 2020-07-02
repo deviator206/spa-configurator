9383eeb3c3c3649a70764d3649deb50ac1ed8f68
deviator206 <deviator206@gmail.com>
2020-07-02 09:40:19 +0530

    draft version

--- app-react-webpack/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const element = <h1>asdasds asd React! New Wonasdasdsader ahhha!!</h1>;
-
-ReactDOM.render(
-    element,  
-    document.getElementById('root')
-  );
